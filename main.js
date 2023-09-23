7469fc87122993b9c75b2fde1afa2f2b96619041
Tamás Kis <t.kis502@gmail.com>
2023-09-23 09:58:19 +0200

    the functions are ready and the code runs once

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ function generateSecret() {
 }
 
 function readGuess() {
-
+    let guess =
+        prompt("Találd ki a titkos kódot, ami négy számjegyből áll!")
+            .split("");
+    return guess;
 }
 
 function getBlackCount(guess, secret) {
@@ -14,11 +17,18 @@ function getBlackCount(guess, secret) {
             egyezésekSzáma++;
         }
     }
+
     return egyezésekSzáma;
 }
 
 function getWhiteCount(guess, secret, blackCount) {
-
+    let egyezésekSzáma = 0;
+    for (let i = 0; i < guess.length; i++) {
+        if (guess.sort()[i] === secret.sort()[i]) {
+            egyezésekSzáma++;
+        }
+    }
+    return egyezésekSzáma - blackCount;
 }
 
 function isGameWon(blackCount) {
@@ -27,9 +37,13 @@ function isGameWon(blackCount) {
 
 function gameLoop() {   // levezényli a játékot
     const secret = generateSecret();    // legenerál egy négyszámjegyű sorozatot
+    console.log(secret);
     let guess = readGuess();    // bekér a felhasználótól egy tippet
+    console.log(guess);
     let blackCount = getBlackCount(guess, secret);   // fekete pöttyök száma
+    console.log(blackCount);
     let whiteCount = getWhiteCount(guess, secret, blackCount);   // fehér pöttyök száma
+    console.log(whiteCount);
 
     if (isGameWon(blackCount)) {
         console.log("Nyertél!");    // nyertünk?
